0341f7a714cac46287ba8a731aa7b80739864340
Anian Ruoss <anian.ruoss@gmail.com>
2018-10-14 19:04:47 +0200

    Plot loss frequency from PGD attacks

--- exercise04/task.py
+++ exercise04/task.py
@@ -1,18 +1,20 @@
 import time
-from os import path
+from os import makedirs, path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from tensorboardX import SummaryWriter
 from torchvision import datasets, transforms
 
 PROJECT_ROOT = path.dirname(path.dirname(path.abspath(__file__)))
 
-use_cuda = False
-device = torch.device("cuda" if use_cuda else "cpu")
+if not path.isdir(path.join(PROJECT_ROOT, 'exercise04/plots')):
+    makedirs(path.join(PROJECT_ROOT, 'exercise04/plots'))
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 batch_size = 64
 
 np.random.seed(42)
@@ -60,9 +62,14 @@ model = Net()
 model = model.to(device)
 model.train()
 
+# PGD parameters
+eps = 0.1
+eps_step = 0.01
+steps = 40
+
 
 def fgsm(model, x, target, eps, targeted=True, clip_min=None, clip_max=None):
-    input = x.clone().detach()
+    input = x.clone().detach().to(device)
     input.requires_grad_()
 
     logits = model(input)
@@ -107,14 +114,17 @@ if not path.isfile('model.pt'):
 
     ce_loss = torch.nn.CrossEntropyLoss()
 
-    writer = SummaryWriter()
     tot_steps = 0
+
+    rand_train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=1, shuffle=True
+    )
 
     for epoch in range(1, num_epochs + 1):
         t1 = time.time()
         for batch_idx, (x_batch, y_batch) in enumerate(train_loader):
             x_batch = pgd(
-                model, x_batch, y_batch, 40, 0.1, 0.01, targeted=False,
+                model, x_batch, y_batch, steps, eps, eps_step, targeted=False,
                 clip_min=0., clip_max=1.
             )
 
@@ -128,8 +138,26 @@ if not path.isfile('model.pt'):
                 pred = torch.max(out, dim=1)[1]
                 acc = pred.eq(y_batch).sum().item() / float(batch_size)
 
-                writer.add_scalar('data/accuracy', acc, tot_steps)
-                writer.add_scalar('data/loss', batch_loss.item(), tot_steps)
+                # reproduce results from Madry et al. (arXiv:1706.06083)
+                image, label = next(iter(rand_train_loader))
+                x_min = image - eps
+                x_max = image + eps
+
+                perturbed_losses = np.empty(1000)
+
+                for i in range(1000):
+                    start = (x_min - x_max) * torch.rand(image.shape) + x_max
+                    perturbed_image = pgd(
+                        model, start, label, steps, eps, eps_step, clip_min=0.,
+                        clip_max=1.
+                    )
+                    perturbed_losses[i] = ce_loss(model(perturbed_image), label)
+
+                plt.hist(perturbed_losses, label=f'batch {batch_idx}')
+                plt.yscale('log', nonposy='clip')
+                plt.xlabel('Loss value')
+                plt.ylabel('log(frequency)')
+                plt.yticks([])
 
             batch_loss.backward()
             opt.step()
@@ -144,12 +172,34 @@ if not path.isfile('model.pt'):
             tot_test += x_batch.size()[0]
         t2 = time.time()
 
+        plt.legend()
+        plt.savefig(
+            path.join(
+                PROJECT_ROOT, 'exercise04/plots', f'frequency_epoch_{epoch}.png'
+            )
+        )
+        plt.close()
         print('Epoch %d: Accuracy %.5lf [%.2lf seconds]' % (
             epoch, tot_acc / tot_test, t2 - t1))
 
     torch.save(model.state_dict(), 'model.pt')
 
 else:
-    model.load_state_dict(torch.load('model.pt'))
+    model.load_state_dict(
+        torch.load('model.pt', map_location=lambda storage, loc: storage)
+    )
 
 model.eval()
+
+eval_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=1000, shuffle=False
+)
+images, labels = next(iter(eval_loader))
+print('accuracy on unperturbed images:',
+      torch.max(model(images), 1)[1].eq(labels).sum().item() / labels.size()[0])
+images = pgd(
+    model, images, labels, steps, eps, eps_step, targeted=False, clip_min=0.,
+    clip_max=1.
+)
+print('accuracy on perturbed images:',
+      torch.max(model(images), 1)[1].eq(labels).sum().item() / labels.size()[0])
