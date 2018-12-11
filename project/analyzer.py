1bc70b4299d32c98fe204852ef8f173d09acdf80
Anian Ruoss <anian.ruoss@gmail.com>
2018-12-11 18:06:03 +0100

    Refactor

--- project/analyzer.py
+++ project/analyzer.py
@@ -39,7 +39,7 @@ def parse_bias(text):
     if text[-1] != ']':
         raise Exception("expected ']'")
     v = np.array([*map(lambda x: np.double(x.strip()), text[1:-1].split(','))])
-    # return v.reshape((v.size,1))
+
     return v
 
 
@@ -49,8 +49,8 @@ def parse_vector(text):
     if text[-1] != ']':
         raise Exception("expected ']'")
     v = np.array([*map(lambda x: np.double(x.strip()), text[1:-1].split(','))])
+
     return v.reshape((v.size, 1))
-    # return v
 
 
 def balanced_split(text):
@@ -58,6 +58,7 @@ def balanced_split(text):
     bal = 0
     start = 0
     result = []
+
     while i < len(text):
         if text[i] == '[':
             bal += 1
@@ -67,19 +68,22 @@ def balanced_split(text):
             result.append(text[start:i])
             start = i + 1
         i += 1
+
     if start < i:
         result.append(text[start:i])
+
     return result
 
 
 def parse_matrix(text):
-    i = 0
     if len(text) < 1 or text[0] != '[':
         raise Exception("expected '['")
     if text[-1] != ']':
         raise Exception("expected ']'")
-    return np.array([*map(lambda x: parse_vector(x.strip()).flatten(),
-                          balanced_split(text[1:-1]))])
+
+    return np.array([*map(
+        lambda x: parse_vector(x.strip()).flatten(), balanced_split(text[1:-1])
+    )])
 
 
 def parse_net(text):
@@ -105,11 +109,14 @@ def parse_net(text):
 def parse_spec(text):
     text = text.replace("[", "")
     text = text.replace("]", "")
+
     with open('dummy', 'w') as my_file:
         my_file.write(text)
+
     data = np.genfromtxt('dummy', delimiter=',', dtype=np.double)
     low = np.copy(data[:, 0])
     high = np.copy(data[:, 1])
+
     return low, high
 
 
@@ -120,20 +127,24 @@ def get_perturbed_image(x, epsilon):
     UB_N0 = image + epsilon
 
     for i in range(num_pixels):
-        if (LB_N0[i] < 0):
+        if LB_N0[i] < 0:
             LB_N0[i] = 0
-        if (UB_N0[i] > 1):
+        if UB_N0[i] > 1:
             UB_N0[i] = 1
+
     return LB_N0, UB_N0
 
 
 def generate_linexpr0(weights, bias, size):
-    linexpr0 = elina_linexpr0_alloc(ElinaLinexprDiscr.ELINA_LINEXPR_DENSE,
-                                    size)
+    linexpr0 = elina_linexpr0_alloc(
+        ElinaLinexprDiscr.ELINA_LINEXPR_DENSE, size
+    )
     cst = pointer(linexpr0.contents.cst)
     elina_scalar_set_double(cst.contents.val.scalar, bias)
+
     for i in range(size):
         elina_linexpr0_set_coeff_scalar_double(linexpr0, i, weights[i])
+
     return linexpr0
 
 
@@ -141,49 +152,61 @@ def analyze(nn, LB_N0, UB_N0, label):
     num_pixels = len(LB_N0)
     nn.ffn_counter = 0
     numlayer = nn.numlayer
+
     man = elina_box_manager_alloc()
     itv = elina_interval_array_alloc(num_pixels)
+
     for i in range(num_pixels):
         elina_interval_set_double(itv[i], LB_N0[i], UB_N0[i])
 
-    ## construct input abstraction
+    # construct input abstraction
     element = elina_abstract0_of_box(man, 0, num_pixels, itv)
     elina_interval_array_free(itv, num_pixels)
+
     for layerno in range(numlayer):
         if (nn.layertypes[layerno] in ['ReLU', 'Affine']):
             weights = nn.weights[nn.ffn_counter]
             biases = nn.biases[nn.ffn_counter]
+
             dims = elina_abstract0_dimension(man, element)
             num_in_pixels = dims.intdim + dims.realdim
             num_out_pixels = len(weights)
-
             dimadd = elina_dimchange_alloc(0, num_out_pixels)
+
             for i in range(num_out_pixels):
                 dimadd.contents.dim[i] = num_in_pixels
+
             elina_abstract0_add_dimensions(man, True, element, dimadd, False)
             elina_dimchange_free(dimadd)
             np.ascontiguousarray(weights, dtype=np.double)
             np.ascontiguousarray(biases, dtype=np.double)
             var = num_in_pixels
+
             # handle affine layer
             for i in range(num_out_pixels):
                 tdim = ElinaDim(var)
-                linexpr0 = generate_linexpr0(weights[i], biases[i],
-                                             num_in_pixels)
-                element = elina_abstract0_assign_linexpr_array(man, True,
-                                                               element, tdim,
-                                                               linexpr0, 1,
-                                                               None)
+                linexpr0 = generate_linexpr0(
+                    weights[i], biases[i], num_in_pixels
+                )
+                element = elina_abstract0_assign_linexpr_array(
+                    man, True, element, tdim, linexpr0, 1, None
+                )
                 var += 1
+
             dimrem = elina_dimchange_alloc(0, num_in_pixels)
+
             for i in range(num_in_pixels):
                 dimrem.contents.dim[i] = i
+
             elina_abstract0_remove_dimensions(man, True, element, dimrem)
             elina_dimchange_free(dimrem)
+
             # handle ReLU layer
-            if (nn.layertypes[layerno] == 'ReLU'):
-                element = relu_box_layerwise(man, True, element, 0,
-                                             num_out_pixels)
+            if nn.layertypes[layerno] == 'ReLU':
+                element = relu_box_layerwise(
+                    man, True, element, 0, num_out_pixels
+                )
+
             nn.ffn_counter += 1
 
         else:
@@ -195,28 +218,30 @@ def analyze(nn, LB_N0, UB_N0, label):
     bounds = elina_abstract0_to_box(man, element)
 
     # if epsilon is zero, try to classify else verify robustness
-
     verified_flag = True
     predicted_label = 0
-    if (LB_N0[0] == UB_N0[0]):
+
+    if LB_N0[0] == UB_N0[0]:
         for i in range(output_size):
             inf = bounds[i].contents.inf.contents.val.dbl
             flag = True
+
             for j in range(output_size):
-                if (j != i):
+                if j != i:
                     sup = bounds[j].contents.sup.contents.val.dbl
-                    if (inf <= sup):
+                    if inf <= sup:
                         flag = False
                         break
-            if (flag):
+
+            if flag:
                 predicted_label = i
                 break
     else:
         inf = bounds[label].contents.inf.contents.val.dbl
         for j in range(output_size):
-            if (j != label):
+            if j != label:
                 sup = bounds[j].contents.sup.contents.val.dbl
-                if (inf <= sup):
+                if inf <= sup:
                     predicted_label = label
                     verified_flag = False
                     break
@@ -224,6 +249,7 @@ def analyze(nn, LB_N0, UB_N0, label):
     elina_interval_array_free(bounds, output_size)
     elina_abstract0_free(man, element)
     elina_manager_free(man)
+
     return predicted_label, verified_flag
 
 
