1993b1bdc67ffa0adeb2263528ff9e263eddf00f
Anian Ruoss <anian.ruoss@gmail.com>
2018-12-11 16:33:09 +0100

    Refactor

--- project/analyzer.py
+++ project/analyzer.py
@@ -1,23 +1,26 @@
+import csv
+import ctypes
+import re
 import sys
+import time
+from ctypes.util import find_library
+
+import numpy as np
+
+from gurobipy import *
 
 sys.path.insert(0, '../ELINA/python_interface/')
 
-import numpy as np
-import re
-import csv
-from elina_box import *
-from elina_interval import *
 from elina_abstract0 import *
-from elina_manager import *
+from elina_box import *
 from elina_dimension import *
-from elina_scalar import *
 from elina_interval import *
-from elina_linexpr0 import *
 from elina_lincons0 import *
-import ctypes
-from ctypes.util import find_library
-from gurobipy import *
-import time
+from elina_linexpr0 import *
+from elina_manager import *
+from elina_scalar import *
+
+
 
 libc = CDLL(find_library('c'))
 cstdout = c_void_p.in_dll(libc, 'stdout')
