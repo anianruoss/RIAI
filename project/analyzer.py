767d7e6df128099df8e3c0eabcff54ea62304b7b
Anian Ruoss <anian.ruoss@gmail.com>
2018-12-11 16:29:55 +0100

    Refactor

--- project/analyzer.py
+++ project/analyzer.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.insert(0, '../ELINA/python_interface/')
 
+sys.path.insert(0, '../ELINA/python_interface/')
 
 import numpy as np
 import re
@@ -22,6 +22,7 @@ import time
 libc = CDLL(find_library('c'))
 cstdout = c_void_p.in_dll(libc, 'stdout')
 
+
 class layers:
     def __init__(self):
         self.layertypes = []
@@ -30,14 +31,16 @@ class layers:
         self.numlayer = 0
         self.ffn_counter = 0
 
+
 def parse_bias(text):
     if len(text) < 1 or text[0] != '[':
         raise Exception("expected '['")
     if text[-1] != ']':
         raise Exception("expected ']'")
     v = np.array([*map(lambda x: np.double(x.strip()), text[1:-1].split(','))])
-    #return v.reshape((v.size,1))
+    # return v.reshape((v.size,1))
     return v
+
 
 def parse_vector(text):
     if len(text) < 1 or text[0] != '[':
@@ -45,8 +48,9 @@ def parse_vector(text):
     if text[-1] != ']':
         raise Exception("expected ']'")
     v = np.array([*map(lambda x: np.double(x.strip()), text[1:-1].split(','))])
-    return v.reshape((v.size,1))
-    #return v
+    return v.reshape((v.size, 1))
+    # return v
+
 
 def balanced_split(text):
     i = 0
@@ -60,11 +64,12 @@ def balanced_split(text):
             bal -= 1
         elif text[i] == ',' and bal == 0:
             result.append(text[start:i])
-            start = i+1
+            start = i + 1
         i += 1
     if start < i:
         result.append(text[start:i])
     return result
+
 
 def parse_matrix(text):
     i = 0
@@ -72,7 +77,9 @@ def parse_matrix(text):
         raise Exception("expected '['")
     if text[-1] != ']':
         raise Exception("expected ']'")
-    return np.array([*map(lambda x: parse_vector(x.strip()).flatten(), balanced_split(text[1:-1]))])
+    return np.array([*map(lambda x: parse_vector(x.strip()).flatten(),
+                          balanced_split(text[1:-1]))])
+
 
 def parse_net(text):
     lines = [*filter(lambda x: len(x) != 0, text.split('\n'))]
@@ -80,138 +87,146 @@ def parse_net(text):
     res = layers()
     while i < len(lines):
         if lines[i] in ['ReLU', 'Affine']:
-            W = parse_matrix(lines[i+1])
-            b = parse_bias(lines[i+2])
+            W = parse_matrix(lines[i + 1])
+            b = parse_bias(lines[i + 2])
             res.layertypes.append(lines[i])
             res.weights.append(W)
             res.biases.append(b)
-            res.numlayer+= 1
+            res.numlayer += 1
             i += 3
         else:
-            raise Exception('parse error: '+lines[i])
+            raise Exception('parse error: ' + lines[i])
     return res
-   
+
+
 def parse_spec(text):
     text = text.replace("[", "")
     text = text.replace("]", "")
     with open('dummy', 'w') as my_file:
         my_file.write(text)
-    data = np.genfromtxt('dummy', delimiter=',',dtype=np.double)
-    low = np.copy(data[:,0])
-    high = np.copy(data[:,1])
-    return low,high
+    data = np.genfromtxt('dummy', delimiter=',', dtype=np.double)
+    low = np.copy(data[:, 0])
+    high = np.copy(data[:, 1])
+    return low, high
+
 
 def get_perturbed_image(x, epsilon):
     image = x[1:len(x)]
     num_pixels = len(image)
     LB_N0 = image - epsilon
     UB_N0 = image + epsilon
-     
+
     for i in range(num_pixels):
-        if(LB_N0[i] < 0):
+        if (LB_N0[i] < 0):
             LB_N0[i] = 0
-        if(UB_N0[i] > 1):
+        if (UB_N0[i] > 1):
             UB_N0[i] = 1
     return LB_N0, UB_N0
 
 
 def generate_linexpr0(weights, bias, size):
-    linexpr0 = elina_linexpr0_alloc(ElinaLinexprDiscr.ELINA_LINEXPR_DENSE, size)
+    linexpr0 = elina_linexpr0_alloc(ElinaLinexprDiscr.ELINA_LINEXPR_DENSE,
+                                    size)
     cst = pointer(linexpr0.contents.cst)
     elina_scalar_set_double(cst.contents.val.scalar, bias)
     for i in range(size):
-        elina_linexpr0_set_coeff_scalar_double(linexpr0,i,weights[i])
+        elina_linexpr0_set_coeff_scalar_double(linexpr0, i, weights[i])
     return linexpr0
 
-def analyze(nn, LB_N0, UB_N0, label):   
+
+def analyze(nn, LB_N0, UB_N0, label):
     num_pixels = len(LB_N0)
     nn.ffn_counter = 0
-    numlayer = nn.numlayer 
+    numlayer = nn.numlayer
     man = elina_box_manager_alloc()
     itv = elina_interval_array_alloc(num_pixels)
     for i in range(num_pixels):
-        elina_interval_set_double(itv[i],LB_N0[i],UB_N0[i])
+        elina_interval_set_double(itv[i], LB_N0[i], UB_N0[i])
 
     ## construct input abstraction
     element = elina_abstract0_of_box(man, 0, num_pixels, itv)
-    elina_interval_array_free(itv,num_pixels)
+    elina_interval_array_free(itv, num_pixels)
     for layerno in range(numlayer):
-        if(nn.layertypes[layerno] in ['ReLU', 'Affine']):
-           weights = nn.weights[nn.ffn_counter]
-           biases = nn.biases[nn.ffn_counter]
-           dims = elina_abstract0_dimension(man,element)
-           num_in_pixels = dims.intdim + dims.realdim
-           num_out_pixels = len(weights)
+        if (nn.layertypes[layerno] in ['ReLU', 'Affine']):
+            weights = nn.weights[nn.ffn_counter]
+            biases = nn.biases[nn.ffn_counter]
+            dims = elina_abstract0_dimension(man, element)
+            num_in_pixels = dims.intdim + dims.realdim
+            num_out_pixels = len(weights)
 
-           dimadd = elina_dimchange_alloc(0,num_out_pixels)    
-           for i in range(num_out_pixels):
-               dimadd.contents.dim[i] = num_in_pixels
-           elina_abstract0_add_dimensions(man, True, element, dimadd, False)
-           elina_dimchange_free(dimadd)
-           np.ascontiguousarray(weights, dtype=np.double)
-           np.ascontiguousarray(biases, dtype=np.double)
-           var = num_in_pixels
-           # handle affine layer
-           for i in range(num_out_pixels):
-               tdim= ElinaDim(var)
-               linexpr0 = generate_linexpr0(weights[i],biases[i],num_in_pixels)
-               element = elina_abstract0_assign_linexpr_array(man, True, element, tdim, linexpr0, 1, None)
-               var+=1
-           dimrem = elina_dimchange_alloc(0,num_in_pixels)
-           for i in range(num_in_pixels):
-               dimrem.contents.dim[i] = i
-           elina_abstract0_remove_dimensions(man, True, element, dimrem)
-           elina_dimchange_free(dimrem)
-           # handle ReLU layer 
-           if(nn.layertypes[layerno]=='ReLU'):
-              element = relu_box_layerwise(man,True,element,0, num_out_pixels)
-           nn.ffn_counter+=1 
+            dimadd = elina_dimchange_alloc(0, num_out_pixels)
+            for i in range(num_out_pixels):
+                dimadd.contents.dim[i] = num_in_pixels
+            elina_abstract0_add_dimensions(man, True, element, dimadd, False)
+            elina_dimchange_free(dimadd)
+            np.ascontiguousarray(weights, dtype=np.double)
+            np.ascontiguousarray(biases, dtype=np.double)
+            var = num_in_pixels
+            # handle affine layer
+            for i in range(num_out_pixels):
+                tdim = ElinaDim(var)
+                linexpr0 = generate_linexpr0(weights[i], biases[i],
+                                             num_in_pixels)
+                element = elina_abstract0_assign_linexpr_array(man, True,
+                                                               element, tdim,
+                                                               linexpr0, 1,
+                                                               None)
+                var += 1
+            dimrem = elina_dimchange_alloc(0, num_in_pixels)
+            for i in range(num_in_pixels):
+                dimrem.contents.dim[i] = i
+            elina_abstract0_remove_dimensions(man, True, element, dimrem)
+            elina_dimchange_free(dimrem)
+            # handle ReLU layer
+            if (nn.layertypes[layerno] == 'ReLU'):
+                element = relu_box_layerwise(man, True, element, 0,
+                                             num_out_pixels)
+            nn.ffn_counter += 1
 
         else:
-           print(' net type not supported')
-   
-    dims = elina_abstract0_dimension(man,element)
+            print(' net type not supported')
+
+    dims = elina_abstract0_dimension(man, element)
     output_size = dims.intdim + dims.realdim
     # get bounds for each output neuron
-    bounds = elina_abstract0_to_box(man,element)
+    bounds = elina_abstract0_to_box(man, element)
 
-           
-    # if epsilon is zero, try to classify else verify robustness 
-    
+    # if epsilon is zero, try to classify else verify robustness
+
     verified_flag = True
     predicted_label = 0
-    if(LB_N0[0]==UB_N0[0]):
+    if (LB_N0[0] == UB_N0[0]):
         for i in range(output_size):
             inf = bounds[i].contents.inf.contents.val.dbl
             flag = True
             for j in range(output_size):
-                if(j!=i):
-                   sup = bounds[j].contents.sup.contents.val.dbl
-                   if(inf<=sup):
-                      flag = False
-                      break
-            if(flag):
+                if (j != i):
+                    sup = bounds[j].contents.sup.contents.val.dbl
+                    if (inf <= sup):
+                        flag = False
+                        break
+            if (flag):
                 predicted_label = i
-                break    
+                break
     else:
         inf = bounds[label].contents.inf.contents.val.dbl
         for j in range(output_size):
-            if(j!=label):
+            if (j != label):
                 sup = bounds[j].contents.sup.contents.val.dbl
-                if(inf<=sup):
+                if (inf <= sup):
                     predicted_label = label
                     verified_flag = False
                     break
 
-    elina_interval_array_free(bounds,output_size)
-    elina_abstract0_free(man,element)
-    elina_manager_free(man)        
+    elina_interval_array_free(bounds, output_size)
+    elina_abstract0_free(man, element)
+    elina_manager_free(man)
     return predicted_label, verified_flag
-
 
 
 if __name__ == '__main__':
     from sys import argv
+
     if len(argv) < 3 or len(argv) > 4:
         print('usage: python3.6 ' + argv[0] + ' net.txt spec.txt [timeout]')
         exit(1)
@@ -219,27 +234,26 @@ if __name__ == '__main__':
     netname = argv[1]
     specname = argv[2]
     epsilon = float(argv[3])
-    #c_label = int(argv[4])
+    # c_label = int(argv[4])
     with open(netname, 'r') as netfile:
         netstring = netfile.read()
     with open(specname, 'r') as specfile:
         specstring = specfile.read()
     nn = parse_net(netstring)
     x0_low, x0_high = parse_spec(specstring)
-    LB_N0, UB_N0 = get_perturbed_image(x0_low,0)
-    
-    label, _ = analyze(nn,LB_N0,UB_N0,0)
+    LB_N0, UB_N0 = get_perturbed_image(x0_low, 0)
+
+    label, _ = analyze(nn, LB_N0, UB_N0, 0)
     start = time.time()
-    if(label==int(x0_low[0])):
-        LB_N0, UB_N0 = get_perturbed_image(x0_low,epsilon)
-        _, verified_flag = analyze(nn,LB_N0,UB_N0,label)
-        if(verified_flag):
+    if (label == int(x0_low[0])):
+        LB_N0, UB_N0 = get_perturbed_image(x0_low, epsilon)
+        _, verified_flag = analyze(nn, LB_N0, UB_N0, label)
+        if (verified_flag):
             print("verified")
         else:
-            print("can not be verified")  
+            print("can not be verified")
     else:
-        print("image not correctly classified by the network. expected label ",int(x0_low[0]), " classified label: ", label)
+        print("image not correctly classified by the network. expected label ",
+              int(x0_low[0]), " classified label: ", label)
     end = time.time()
-    print("analysis time: ", (end-start), " seconds")
-    
-
+    print("analysis time: ", (end - start), " seconds")
--- project/analyzer/setup_gurobi.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export GUROBI_HOME=/home/riai2018/gurobi810/linux64
-export PATH=${PATH}:${GUROBI_HOME}/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib
